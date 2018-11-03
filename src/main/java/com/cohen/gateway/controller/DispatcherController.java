cd55cd5e46a1660b5b3d247faee6dd5a9d0e8dff
cohen <18264180308@163.com>
2018-11-03 17:17:22 +0800

    修改eureka配置

--- src/main/java/com/cohen/gateway/controller/DispatcherController.java
+++ src/main/java/com/cohen/gateway/controller/DispatcherController.java
@@ -1,18 +1,28 @@
 package com.cohen.gateway.controller;
 
+import com.netflix.appinfo.InstanceInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @RestController
 public class DispatcherController {
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @Autowired
     private RestTemplate restTemplate;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("WECHAT-APP-SERVER");
         return restTemplate.getForEntity("http://WECHAT-APP-SERVER/hello", String.class).getBody();
     }
 }
