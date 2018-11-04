9a24f8d67dc0978c3caddc411d33a91f786c147a
cohen <18264180308@163.com>
2018-11-04 20:32:16 +0800

    增加zuul配置

--- src/main/java/com/cohen/gateway/GatewayApplication.java
+++ src/main/java/com/cohen/gateway/GatewayApplication.java
@@ -5,18 +5,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 @EnableEurekaServer
 @EnableDiscoveryClient
+@EnableZuulProxy
 public class GatewayApplication {
 
     @Bean
     @LoadBalanced
-    RestTemplate rest() {
-        return new RestTemplate();
+    RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+        return restTemplate;
     }
 
     public static void main(String[] args) {
