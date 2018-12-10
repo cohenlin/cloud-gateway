a5cace2a3eb547c6cbad8c8ce353b1d1b7be63a8
linjincheng <ljc@choicesoft.com.cn>
2018-12-10 14:39:35 +0800

    增加测试接口

--- src/main/java/com/cohen/gateway/TestController.java
+++ src/main/java/com/cohen/gateway/TestController.java
@@ -3,6 +3,9 @@ package com.cohen.gateway;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: linjincheng
  * @createTime: Created in 2018/12/10 12:57
@@ -15,6 +18,10 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        return ServerResponse.createBySuccess("hello");
+        List<User> list = new ArrayList<>();
+        list.add(new User("刘德华", 18, "西湖区湖底公园1号"));
+        list.add(new User("吴彦祖", 19, "西湖区湖底公园2号"));
+        list.add(new User("周润发", 20, "西湖区湖底公园3号"));
+        return ServerResponse.createBySuccess(list);
     }
 }
